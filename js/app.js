ce2017486b9ef3763e5a847377f11f17b25fc145
Ching-Hung Lin <okeydoky@gmail.com>
2015-02-14 20:16:36 -0500

    First working sample; drawing bug

--- js/app.js
+++ js/app.js
@@ -6,6 +6,8 @@ var Enemy = function() {
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    this.x = 101;
+    this.y = 146;
 }
 
 // Update the enemy's position, required method for game
@@ -24,12 +26,27 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function() {
 
+}
+
+Player.prototype.update = function(dt) {
+
+}
+
+Player.prototype.render = function() {
+
+}
+
+Player.prototype.handleInput = function() {
+
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
+var allEnemies = [new Enemy(), new Enemy()];
+var player = new Player();
 
 
 // This listens for key presses and sends the keys to your
