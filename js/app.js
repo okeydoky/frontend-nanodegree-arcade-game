eb647eff2906c01e4ab0c01bbf0b7797fadeb24a
Ching-Hung Lin <okeydoky@gmail.com>
2015-02-15 09:42:15 -0500

    Add settings to avoid hardcode. Add Player class

--- js/app.js
+++ js/app.js
@@ -1,3 +1,11 @@
+// Game settings
+var settings = {
+    board: {col: 5, row: 6},
+    tile: {width: 101, height: 83},
+    enemy: {movement: 60,   // px, the greater the faster
+            count: 5}
+};
+
 // Enemies our player must avoid
 var Enemy = function() {
     // Variables applied to each of our instances go here,
@@ -15,7 +23,7 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-    this.x += 60 * this.speed * dt;
+    this.x += settings.enemy.movement * this.speed * dt;
 
     if (this.x > ctx.canvas.width) {
         this.init();
@@ -31,15 +39,16 @@ Enemy.prototype.render = function() {
 // Init/Reset the enemy position
 Enemy.prototype.init = function() {
     this.speed = getRandomInt(1, 6);  // 5 possible speed
-    this.x = -202;
-    this.y = 63 + getRandomInt(0, 3) * 83;  // random row between 1-3
+    this.x = -2 * settings.tile.width;
+    this.y = 63 + getRandomInt(0, 3) * settings.tile.height;  // random row between 1-3
 }
 
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
 var Player = function() {
-
+    this.sprite = 'images/char-boy.png';
+    this.init();
 }
 
 Player.prototype.update = function(dt) {
@@ -47,11 +56,42 @@ Player.prototype.update = function(dt) {
 }
 
 Player.prototype.render = function() {
-
+    var x = this.col * settings.tile.width;
+    var y = this.row * settings.tile.height - 10;
+    ctx.drawImage(Resources.get(this.sprite), x, y);
 }
 
-Player.prototype.handleInput = function() {
+Player.prototype.handleInput = function(direction) {
+    switch (direction) {
+        case 'left':
+            if (this.col > 0) { 
+                this.col--;
+            }
+            break;
+        case 'up' :
+            if (this.row > 0) {
+                this.row--;
+            }
+            break;
+        case 'right':
+            if (this.col < settings.board.col - 1) {    // 0-index based
+                this.col++;
+            }
+            break;
+        case 'down':
+            if (this.row < settings.board.row - 1) {    // 0-index based
+                this.row++;
+            }
+            break;
+        default:
 
+    }
+}
+
+// Init/Reset player position
+Player.prototype.init = function() {
+    this.col = Math.floor(settings.board.col / 2);
+    this.row = settings.board.row - 1;
 }
 
 // Now instantiate your objects.
@@ -59,7 +99,7 @@ Player.prototype.handleInput = function() {
 // Place the player object in a variable called player
 
 var allEnemies = new Array();
-for (var i = 0; i < 5; ++i) {   // maximum 6 enemies
+for (var i = 0; i < settings.enemy.count; ++i) {   
     allEnemies.push(new Enemy());
 }
 
